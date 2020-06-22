b276fdc3b2a0f825d0b07d72e3b93cc094b9b490
Anonymous-coder-its-me <56034618+Anonymous-coder-its-me@users.noreply.github.com>
2020-06-22 16:56:24 +0530

    added lines to check unregistered email

--- apis/login.php
+++ apis/login.php
@@ -7,35 +7,37 @@ $loginPassword=$_POST['loginPassword'];
     if(!empty($loginEmail) && !empty($loginPassword))
     {
 
-		$loginPassword=hash('sha512', $loginPassword);
+		
 
-        $query1 ="SELECT * FROM users WHERE email='$loginEmail' AND password='$loginPassword'";
+        $query1 ="SELECT * FROM users WHERE email='$loginEmail'";
         $query1_run=mysqli_query($conn,$query1);
-        $query_row=mysqli_fetch_array($query1_run);
-        if(mysqli_num_rows($query1_run)>0)
+	if(mysqli_num_rows($query1_run)>0)
         {  
-           
-                $_SESSION['name'] = $query_row['name'];
-                $_SESSION['Email']=$query_row['email'];
-                echo(json_encode(array('status'=>'success','message'=>$_SESSION['name'])));
-                
-            
-        }
-        else
-        {
-            echo(json_encode(array('status'=>'failure','message' => 'Wrong password')));
-        }
-        // }
-        /*else if(mysqli_num_rows($query11_run)>0)
-        {
-            header("Location:hack.php");
-        }*/
+	      $loginPassword=hash('sha512', $loginPassword);
+              $query2 ="SELECT * FROM users WHERE password='$loginPassword'";
+              $query2_run=mysqli_query($conn,$query2);
+              $query_row=mysqli_fetch_array($query2_run);
+              if(mysqli_num_rows($query2_run)>0)
+                {  
+                  $_SESSION['name'] = $query_row['name'];
+                  $_SESSION['Email']=$query_row['email'];
+                  echo(json_encode(array('status'=>'success','message'=>$_SESSION['name'])));
+                 }
+       		   else
+       		   {
+       	 	      echo(json_encode(array('status'=>'failure','message' => 'Wrong password')));
+     	 	    }
+     	  } 
+	    else
+	    {
+   	 	    echo(json_encode(array('status'=>'failure','message' => 'Unregistered email')));
+             }
         
-    }
-    else
-    {
-        echo 'empty';
-    }
+      }
+      else
+      {
+         echo(json_encode(array('status'=>'failure','message' => 'Please fill out all the fields')));
+      }
 
 	mysqli_close($conn);
 	?>
