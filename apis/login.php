e24b884d96e6bb1880492e8d1086bd96d0c9680d
Anonymous-coder-its-me <56034618+Anonymous-coder-its-me@users.noreply.github.com>
2020-06-22 17:15:19 +0530

    to check post request added

--- apis/login.php
+++ apis/login.php
@@ -4,39 +4,35 @@ include 'db.php';
 $loginEmail=$_POST['loginEmail'];
 $loginPassword=$_POST['loginPassword'];
 // echo $email;
-    if(!empty($loginEmail) && !empty($loginPassword))
-    {
+if($_SERVER["REQUEST_METHOD"] === "POST" ){
+  	  if(!empty($loginEmail) && !empty($loginPassword))
+   	 {
+		  $loginPassword=hash('sha512', $loginPassword);
 
-		
-
-        $query1 ="SELECT * FROM users WHERE email='$loginEmail'";
-        $query1_run=mysqli_query($conn,$query1);
-	if(mysqli_num_rows($query1_run)>0)
-        {  
-	      $loginPassword=hash('sha512', $loginPassword);
-              $query2 ="SELECT * FROM users WHERE password='$loginPassword'";
-              $query2_run=mysqli_query($conn,$query2);
-              $query_row=mysqli_fetch_array($query2_run);
-              if(mysqli_num_rows($query2_run)>0)
-                {  
+       		 $query1 ="SELECT * FROM users WHERE email='$loginEmail' AND password='$loginPassword'";
+       		 $query1_run=mysqli_query($conn,$query1);
+		if(mysqli_num_rows($query1_run)>0)
+       		 { 
                   $_SESSION['name'] = $query_row['name'];
                   $_SESSION['Email']=$query_row['email'];
                   echo(json_encode(array('status'=>'success','message'=>$_SESSION['name'])));
                  }
        		   else
        		   {
-       	 	      echo(json_encode(array('status'=>'failure','message' => 'Wrong password')));
+       	 	      echo(json_encode(array('status'=>'failure','message' => 'Wrong password  or unregistered email')));
      	 	    }
      	  } 
 	    else
 	    {
-   	 	    echo(json_encode(array('status'=>'failure','message' => 'Unregistered email')));
+	    
+         echo(json_encode(array('status'=>'failure','message' => 'Please fill out all the fields')));
+   	 	   
              }
         
       }
       else
       {
-         echo(json_encode(array('status'=>'failure','message' => 'Please fill out all the fields')));
+      echo(json_encode(array('status'=>'failure','message' => 'Not a post request')));
       }
 
 	mysqli_close($conn);
