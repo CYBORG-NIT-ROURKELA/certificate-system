7b215361e8928c67ac01d9cae8a0bb2ac6a24287
Anonymous-coder-its-me <56034618+Anonymous-coder-its-me@users.noreply.github.com>
2020-06-30 20:33:38 +0530

    added files via upload
    displays status based on request and approval

---
+++ assets/ajax/certificate.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+    $.ajax({
+        url: '../apis/check_login.php',
+        type: 'POST',
+        success: function(response) {
+            // console.log(response);
+            response = JSON.parse(response);
+            if (response.status == "success") {
+                var request = response.result.request;
+                console.log(request);
+                var approval = response.result.approval;
+                console.log(approval)
+                if (request == 0 && approval == 0) {
+                    $('#status').html('Not requested');
+                    $('#download').hide();
+                } else if (request == 1 && approval == 0) {
+                    $('#status').html('In process');
+                    $('#download').hide();
+                    $('#request').hide();
+                } else if (request == 1 && approval == 1) {
+                    $('#status').html('Approved! You can download your certificate');
+                    $('#request').hide();
+                };
+            } else {
+                console.log(response);
+                $('.status').html(response.result);
+            }
+        }
+    });
+})
+
+
+$('#request').click(() => {
+    var request = 1;
+    $.ajax({
+        url: '../apis/certificate.php',
+        type: 'POST',
+        data: {
+            request: request
+        },
+        success: (response) => {
+            console.log(response.result);
+            var response = JSON.parse(response);
+            if (response.status == "success") {
+                var request = response.result.request;
+                console.log(request);
+                var approval = response.result.approval;
+                console.log(approval)
+                if (request == 0 && approval == 0) {
+                    $('#status').html('Not requested');
+                    $('#download').hide();
+                } else if (request == 1 && approval == 0) {
+                    $('#status').html('In process');
+                    $('#download').hide();
+                    $('#request').hide();
+                } else if (request == 1 && approval == 1) {
+                    $('#status').html('Approved! You can download your certificate');
+                    $('#request').hide();
+                }
+
+            } else {
+                console.log(response);
+                $('.status').html(response.result);
+            }
+        }
+    });
+})
