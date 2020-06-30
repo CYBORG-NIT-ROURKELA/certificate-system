b154a85cec9fb6eed3e8fdd650656f8c078788b0
Anshuman Sandhibigraha <59245156+anshusandhi6@users.noreply.github.com>
2020-06-30 21:19:32 +0530

    initial commit

---
+++ apis/edit.php
@@ -0,0 +1,38 @@
+<?php
+
+    include('db.php');
+    session_start();
+
+
+
+
+
+	if (isset($_SESSION['logged_in']) && isset($_SESSION['user_id']))
+    { 
+    	
+    	$name=$_POST['name'];
+    	$email=$_POST['email'];
+    	$phoneno=$_POST['phoneno'];
+    	$adress=$_POST['adress'];
+    	$rollno=$_POST['rollno'];
+    	$user_id = $_SESSION['user_id'];
+
+    	$query = mysqli_query($conn, "UPDATE users SET name='$name',phoneno='$phoneno',email='$email',adress='$adress' WHERE   id ='".$user_id."'");
+
+    	if($query)
+    	{
+    		echo" <div class='alert alert-screen'>Data done</div>";
+    	}
+    	else
+    	{
+    		echo" <div class='alert alert-screen'>Error</div>";
+    	}
+    }
+    else
+    {
+    	echo" <div class='alert alert-screen'>Error Again</div>";
+    }
+
+
+
+?>
