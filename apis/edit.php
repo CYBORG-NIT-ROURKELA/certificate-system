ae570b2cb846ee9fa9b55656cf86515b1f69b2af
Anshuman Sandhibigraha <59245156+anshusandhi6@users.noreply.github.com>
2020-07-01 12:53:50 +0530

    Second Commit
    Have attached features like sweet alert and added validation to fields in edit page.

--- apis/edit.php
+++ apis/edit.php
@@ -7,30 +7,33 @@
 
 
 
-	if (isset($_SESSION['logged_in']) && isset($_SESSION['user_id']))
+
+    if (isset($_SESSION['logged_in']) && isset($_SESSION['user_id']))
     { 
-    	
-    	$name=$_POST['name'];
-    	$email=$_POST['email'];
-    	$phoneno=$_POST['phoneno'];
-    	$adress=$_POST['adress'];
-    	$rollno=$_POST['rollno'];
-    	$user_id = $_SESSION['user_id'];
 
-    	$query = mysqli_query($conn, "UPDATE users SET name='$name',phoneno='$phoneno',email='$email',adress='$adress' WHERE   id ='".$user_id."'");
+        
+        $name=$_POST['name'];
+        
+        $phoneno=$_POST['phoneno'];
+        $adress=$_POST['adress'];
+        $rollno=$_POST['rollno'];
+        $user_id = $_SESSION['user_id'];
+      
+$query = mysqli_query($conn, "UPDATE users SET name='$name',phoneno='$phoneno',rollno='$rollno',adress='$adress' WHERE   id ='".$user_id."'");
+        
 
-    	if($query)
-    	{
-    		echo" <div class='alert alert-screen'>Data done</div>";
-    	}
-    	else
-    	{
-    		echo" <div class='alert alert-screen'>Error</div>";
-    	}
+        if($query)
+        {
+            echo" <div class='alert alert-screen'>Data done</div>";
+        }
+        else
+        {
+            echo" <div class='alert alert-screen'>Error</div>";
+        }
     }
     else
     {
-    	echo" <div class='alert alert-screen'>Error Again</div>";
+        echo" <div class='alert alert-screen'>Error Again</div>";
     }
 
 
