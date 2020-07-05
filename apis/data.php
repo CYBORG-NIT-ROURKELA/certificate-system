4ccd6aa63e603cbbe68d0474d581d8f975859b88
Anshuman Sandhibigraha <59245156+anshusandhi6@users.noreply.github.com>
2020-07-05 12:10:48 +0530

    Admin Page Commit
    Have created a Admin Page System where admin can approve the certificate request . Any other user except the admin cannot access the response page and also the user admin cannot access the profile pages.

---
+++ apis/data.php
@@ -0,0 +1,53 @@
+<?php
+
+
+
+
+  
+     function get_data()
+     {
+
+    $conn = mysqli_connect("localhost", "root","","userinfo");
+     $res="1";
+     $app="0";
+    $query = mysqli_query($conn, "SELECT * FROM users WHERE request='$res'AND approval='$app'");
+    
+    $data = array();
+  
+    while ($row = mysqli_fetch_assoc($query))
+   {
+       
+       
+       
+
+       $data[] = array( 
+
+        'id' => $row['id'],
+        'name' => $row['name'],
+        'rollno' => $row['rollno'],
+        'request' => $row['request']
+
+
+
+       );
+
+    }
+   
+  
+ 
+  return  json_encode($data);
+}
+
+
+$file_name = 'abc'.'.json';
+if(file_put_contents($file_name, get_data()))
+{
+  
+}
+else
+{
+  
+}
+
+
+?>
